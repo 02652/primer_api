13e253ff5d3c211f25d1d8872d7b344e6bf27990
02652 <0233023e@gmail.com>
2023-04-15 16:57:02 -0500

    Delete secret key generate

--- drfsimplecrud/settings.py
+++ drfsimplecrud/settings.py
@@ -10,7 +10,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+s@mh28$0daz*!+b%k)nhie#%p0wwq#3t8_i7kt(n)c!dofaxu'
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
